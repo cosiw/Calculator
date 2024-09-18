a7b6c25ccae66810a07279eeb71ecebee0dabecd
cosiw <cosiw@naver.com>
2024-09-18 23:06:45 +0900

    초기 커밋.

---
+++ Calculator/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator
+{
+    public class MainViewModel : ViewModelBase
+    {
+        static decimal result = 3;
+
+        private string process = "";
+
+
+        public decimal Result 
+        { 
+            get { return result; } 
+            set 
+            { 
+                result = value;
+                OnPropertyChanged(nameof(Result));
+            } 
+        }
+
+        public string Process
+        { 
+            get { return process; } 
+            set { process = value; } 
+        }
+        public ICommand PlusCommand { get; }
+
+        public MainViewModel()
+        {
+            PlusCommand = new RelayCommand(plus);
+        }
+        public void plus()
+        {
+            Result += 1;
+        }
+
+    }
+}
