b9bc56bd838f8dad64ac42a79f39ff5c09077b5a
AC933040 <AC933040@AC933040>
2024-10-02 14:56:26 +0900

    연산자 Enum 추가, + 계산 개발

--- Calculator/MainViewModel.cs
+++ Calculator/MainViewModel.cs
@@ -12,35 +12,118 @@ namespace Calculator
 {
     public class MainViewModel : ViewModelBase
     {
-        static decimal result = 3;
+        /// <summary>
+        /// 결과값
+        /// </summary>
+        private decimal mResult = 0;
 
-        private string process = "";
+        /// <summary>
+        /// 이전 입력값
+        /// </summary>
+        private decimal mInput = 0;
 
+        /// <summary>
+        /// 계산 과정
+        /// </summary>
+        private string mProcess = "";
+
+        /// <summary>
+        /// 연산자
+        /// </summary>
+        private OperatorEnum mOperator = 0;
+
+        /// <summary>
+        /// 연산자 Enum
+        /// </summary>
+        public enum OperatorEnum
+        {
+            Add, Subtract, Multiple, Divide 
+        }
 
         public decimal Result 
         { 
-            get { return result; } 
+            get { return mResult; } 
             set 
-            { 
-                result = value;
+            {
+                mResult = value;
                 OnPropertyChanged(nameof(Result));
             } 
         }
 
+        public decimal Input
+        {
+            get { return mInput; }
+            set
+            {
+                mInput = value;
+                OnPropertyChanged(nameof(Input));
+            }
+        }
+
+        public OperatorEnum Operator
+        {
+            get { return mOperator; }
+            set
+            {
+                mOperator = value;
+                OnPropertyChanged(nameof(Operator));
+            }
+
+        }
+
         public string Process
         { 
-            get { return process; } 
-            set { process = value; } 
+            get { return mProcess; } 
+            set 
+            { 
+                mProcess = value;
+                OnPropertyChanged(nameof(Process));
+            } 
         }
+
         public ICommand PlusCommand { get; }
+        
+        public ICommand CalculateCommand { get; }
 
         public MainViewModel()
         {
             PlusCommand = new RelayCommand(plus);
+            CalculateCommand = new RelayCommand(calculate);
         }
         public void plus()
         {
-            Result += 1;
+            if(Process != null)
+            {
+                Process = "";
+            }
+
+            Process += mResult + " + ";
+
+            
+            mOperator = OperatorEnum.Add;
+            //mProcess += mResult + " + ";
+            
+            Input = Result;
+            Result = 0;
+            
+        }
+
+        public void calculate()
+        {
+            switch (Operator)
+            {
+                case OperatorEnum.Add:
+                    {
+                        Process += mResult + " = ";
+                        Result = mResult + mInput; 
+                        break;
+                    }
+
+                case OperatorEnum.Subtract: Result = mResult - mInput; break;
+                case OperatorEnum.Multiple: Result = mResult * mInput; break;
+                case OperatorEnum.Divide: Result = mResult / mInput; break;
+            }
+            Operator = 0;
         }
 
     }
